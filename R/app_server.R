881c7ae2ba292fb429c36e179d59efa79b6be0ae
brentscott93 <bdscott@umass.edu>
2020-07-10 17:46:58 -0400

    add import

--- R/app_server.R
+++ R/app_server.R
@@ -2,7 +2,7 @@
 #' 
 #' @param input,output,session Internal parameters for {shiny}. 
 #'     DO NOT REMOVE.
-#' @import shiny 
+#' @import shiny tidyverse
 #' @importFrom magrittr "%<>%"
 #' @noRd
 app_server <- function( input, output, session ) {
