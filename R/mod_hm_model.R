6aef55461484351a6d24536f72dbf4d929c75382
brentscott93 <bdscott@umass.edu>
2020-09-16 15:31:02 -0400

    update info table

--- R/mod_hm_model.R
+++ R/mod_hm_model.R
@@ -374,7 +374,11 @@ mod_hm_model_server <- function(input, output, session, f){
     defend_if_empty(f$date, ui = 'Please select a date folder', type = 'error')
     showNotification('Refreshing table', type = 'message')
     files <- list_files(f$date$path, pattern = 'trap-data.csv', recursive = T)
-    trap_data <- purrr::map(files$path, vroom::vroom, delim = ",")
+    map_df(files$path, ~vroom::vroom(., delim = ",",
+                                     col_select = c(obs, include, analyzer, report, review),
+                                     n_max = 1))
+    
+   #  trap_data <- purrr::map(files$path, vroom::vroom, delim = ",")
     # if('hm_overlay' %not_in% colnames(trap_data)){
     #  t <- trap_data  %>% 
     #   tidyr::nest(data = c(raw_bead, processed_bead)) 
@@ -382,14 +386,14 @@ mod_hm_model_server <- function(input, output, session, f){
     #   t <- trap_data  %>% 
     #     tidyr::nest(data = c(raw_bead, processed_bead, hm_overlay)) 
     # }
-    
-    purrr::map_dfr(trap_data, function(trap_data){
-      tibble::tibble(obs = unique(trap_data$obs), 
-                     include = unique(trap_data$include),
-                     analyzer = unique(trap_data$analyzer),
-                     report = unique(trap_data$report),
-                     review = unique(trap_data$review))
-    }
+    # 
+    # purrr::map_dfr(trap_data, function(trap_data){
+    #   tibble::tibble(obs = unique(trap_data$obs), 
+    #                  include = unique(trap_data$include),
+    #                  analyzer = unique(trap_data$analyzer),
+    #                  report = unique(trap_data$report),
+    #                  review = unique(trap_data$review))
+    #}
     )
   })
   
