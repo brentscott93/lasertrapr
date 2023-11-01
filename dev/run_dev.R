90d5c7be3bd2536157875364e259b56330fb362e
Brent Scott <brentscott@mailfence.com>
2023-11-01 14:38:12 -0500

    temp change to run_dev

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -16,3 +16,32 @@ golem::document_and_reload()
 run_app()
 
 ## shiny::reactlogShow()
+
+#rename project and conditions
+## trap_data_files <- list.files("~/lasertrapr/project_lasertrapr-paper_mini-ensemble/25_ug-ml",
+##                               pattern = "trap-data.csv",
+##                               full.names = TRUE,
+##                               recursive = TRUE)
+
+
+
+## options_files <- list.files("~/lasertrapr/project_lasertrapr-paper_mini-ensemble/25_ug-ml",
+##                               pattern = "options.csv",
+##                               full.names = TRUE,
+##                               recursive = TRUE)
+
+
+## file_list <- c(trap_data_files, options_files)
+
+## for(i in seq_along(file_list)){
+##   print(i)
+##   fname <- file_list[[i]]
+##   f <- data.table::fread(fname)
+##   ## f[,
+##   ##   ## project = "project_lasertrapr-paper_mini-ensemble",
+##   ##   ## conditions = "25_ug-ml",
+##   ##   date  := "2022-02-22"
+##   ## ]
+##   f$date <- "2022-02-22"
+##   data.table::fwrite(f, fname)
+## }
