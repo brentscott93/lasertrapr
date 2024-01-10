bd32da95b76270ec7636ec9b91e948788f5dba00
Brent Scott <brentscott@mailfence.com>
2024-01-10 15:03:21 -0600

    updates

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -17,30 +17,38 @@ run_app()
 ## shiny::reactlogShow()
 
 #rename project and conditions
-## trap_data_files <- list.files("~/lasertrapr/project_lasertrapr-paper_mini-ensemble/25_ug-ml",
+## trap_data_files <- list.files("~/lasertrapr/project_dani-1mM-atp/10uM-atp",
 ##                               pattern = "trap-data.csv",
 ##                               full.names = TRUE,
 ##                               recursive = TRUE)
 
 
 
-## options_files <- list.files("~/lasertrapr/project_lasertrapr-paper_mini-ensemble/25_ug-ml",
+## options_files <- list.files("~/lasertrapr/project_dani-1mM-atp/10uM-atp",
 ##                               pattern = "options.csv",
 ##                               full.names = TRUE,
 ##                               recursive = TRUE)
 
 
-## file_list <- c(trap_data_files, options_files)
+## me_files <- list.files("~/lasertrapr/project_dani-1mM-atp/10uM-atp",
+##                               pattern = "measured-events.csv",
+##                               full.names = TRUE,
+##                               recursive = TRUE)
+
+
+## file_list <- c(trap_data_files, options_files, me_files)
 
 ## for(i in seq_along(file_list)){
 ##   print(i)
 ##   fname <- file_list[[i]]
 ##   f <- data.table::fread(fname)
 ##   ## f[,
-##   ##   ## project = "project_lasertrapr-paper_mini-ensemble",
+##   ##   project = "project_dani-single-molecule_1uM-atp",
 ##   ##   ## conditions = "25_ug-ml",
-##   ##   date  := "2022-02-22"
+##   ##   ## date  := "2022-02-22"
 ##   ## ]
-##   f$date <- "2022-02-22"
+##   ## f$date <- "2022-02-22"
+##   ## f$project <- "project_dani-single-molecule_1uM-atp"
+##   f$conditions <- "10uM-dani_1mM-atp"
 ##   data.table::fwrite(f, fname)
 ## }
